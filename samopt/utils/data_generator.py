b0f250a20ce62729183d533833a4a4994ef502a1
o-prosper <oprosper@utk.edu>
2025-11-19 13:13:57 -0500

    Made corrections to CTMC simulator; working correctly now.

--- samopt/utils/data_generator.py
+++ samopt/utils/data_generator.py
@@ -1,81 +1,91 @@
 # data_generator.py
 
-def CTMC(purse):#
+def CTMC(purse):
     import numpy as np
     from copy import copy
 
-    # Set up length of simulation and reporting times.
-    T      = purse.T # reporting times
-    tmin   = purse.tmin
-    tmax   = purse.tmax
-    maxiter = purse.maxiter
-    mod_pars = purse.mod_pars
-    init_cond = purse.init_cond
+    # Unpack setup
+    T            = purse.T              # reporting times, increasing
+    tmin         = purse.tmin
+    tmax         = purse.tmax
+    maxiter      = purse.maxiter
+    mod_pars     = purse.mod_pars
+    init_cond    = purse.init_cond
     trans_matrix = purse.trans_matrix
-    # Set state variables equal to initial condition vector
-    x  = [purse.init_cond] # This will be used to save the state values at the reporting times
-    xt = np.array(init_cond) # This will be used to update the states at each event time
-    
-    # get first observation reporting time beyond tmin.
-    j  = 1
-    Treport = T[j]
-    
-    # start time
-    t  = tmin
-    ii = 0
-    while (t < tmax) and (ii < maxiter):
-        ii += 1    
-        
-        # Calculate the transition rates beta*S*I, alpha*I and store in an array.
-        st = xt[0]
-        it = xt[1]
-        alpha = mod_pars[0]
-        beta = mod_pars[1]
-        rates = np.array([beta*st*it,alpha*it])
 
-        # Sum the rates, and if it is positive, compute the cdf of the transition probabilities,
-        # Choose the time of the next event and which event occurs,
-        # Update the state vector and record the states at the reporting times.
-        # Otherwise, set x to previous x.
-        sum_rates = sum(rates)
-        if sum_rates > 0:
-            
-            # Compute the cdf of the transition probabilities
-            cdf = np.cumsum(rates/sum_rates)
-        
-            #Choose the time of next event
-            k   = np.random.uniform(0,1) #Choose a uniform random number
-            t  += np.random.exponential(1.0/sum_rates)
-        
-            # Choose event by finding where k falls within the array cdf.
-            which_transition = np.where(k<=cdf)[0][0]
-    
-            # Add corresponding row of transition matrix to previous state array x,
-            # but save previous states in x_prev.
-            xt_prev = copy(xt)
-            xt += trans_matrix[which_transition]
-        
-            # if time exceeds next observation reporting time, 
-            # return data of previous state
-            if t > Treport:
-                x.append(list(xt_prev))
-            
-                # get next reporting time
-                j += 1
-                if j < len(T):
-                    Treport = T[j]
-                else:
-                    print('I am breaking at A')
-                    break
-            
-            # if time exceeds last reporting time, we're done!
-            if t > T[-1]:
-                print('I am breaking at B')
+    alpha, beta = mod_pars[0], mod_pars[1]
+
+    # Current CTMC state and time
+    xt = np.array(init_cond, dtype=float)
+    t  = float(tmin)
+
+    # Event history (jump times & post-jump states)
+    event_times  = [t]
+    event_states = [xt.copy()]
+
+    # Values at reporting times
+    X_report = []
+
+    ii = 0  # event counter
+    extinct = False
+
+    # Loop over reporting times
+    # T[0] is usually tmin, so record it immediately
+    for j, t_report in enumerate(T):
+        if j == 0:
+            # state at first reporting time
+            X_report.append(xt.copy())
+            continue
+
+        # Simulate until we CROSS this reporting time or hit extinction / maxiter
+        xt_prev = xt.copy()
+        while (t < t_report) and (ii < maxiter):
+            ii += 1
+
+            S, I = xt[0], xt[1]
+            rates = np.array([beta * S * I, alpha * I], dtype=float)
+            sum_rates = rates.sum()
+
+            if sum_rates <= 0:
+                # No more events possible: process stays constant afterwards
+                extinct = True
                 break
-                
-                
-        else:
-            print('I am breaking at C')
-            break 
-            
-    return np.array(x), xt
+
+            # Gillespie step
+            tau = np.random.exponential(1.0 / sum_rates)
+            k   = np.random.uniform(0.0, 1.0)
+
+            # choose which event
+            cdf = np.cumsum(rates / sum_rates)
+            which = 0 if k <= cdf[0] else 1
+
+            # store previous state, then apply jump
+            xt_prev = xt.copy()
+            t += tau
+            xt += trans_matrix[which]
+
+            # record event time & new state
+            event_times.append(t)
+            event_states.append(xt.copy())
+
+        if extinct or ii >= maxiter:
+            # From now on, state stays at current xt
+            X_report.append(xt.copy())
+            # Fill any remaining reporting times with the same state
+            for _ in range(j + 1, len(T)):
+                X_report.append(xt.copy())
+            break
+
+        # Here we know t >= t_report:
+        # the last event happened at time t > t_report,
+        # so the state AT t_report is xt_prev (just before crossing)
+        X_report.append(xt_prev.copy())
+
+    # Safety: ensure lengths match exactly
+    X_report = X_report[:len(T)]
+    X_report = np.asarray(X_report, dtype=float)
+    event_times  = np.asarray(event_times, dtype=float)
+    event_states = np.asarray(event_states, dtype=float)
+
+    # xt is current state at last simulated event time
+    return X_report, xt, event_times, event_states
